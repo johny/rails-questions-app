362996316975f880feb925a637d014c091b1b17d
Jan Kraus <kraus.jan@gmail.com>
2014-03-09 12:14:20 +0100

    Tweaked user frontend, registrations, signup & profile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,12 @@
 class HomeController < ApplicationController
+
+  # for not logged users
+  def index
+    redirect_to :dashboard if user_signed_in?
+  end
+
+  def dashboard
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Questions::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
-
+  get "/dashboard", to: 'home#dashboard', as: "dashboard"
 
   namespace :admin do
     resources :questions do
